628451a2b4efece563c3d0635c8cd41ab2491f42
br3sc4 <30345822+br3sc4@users.noreply.github.com>
2021-05-11 15:44:31 +0200

    Fix logout

--- logout.php
+++ logout.php
@@ -1,9 +1,20 @@
 <?php
+require_once __DIR__.DIRECTORY_SEPARATOR."php".DIRECTORY_SEPARATOR."UserMenu.php";
+
 session_start();
 
 $html = file_get_contents(__DIR__.DIRECTORY_SEPARATOR."pages".DIRECTORY_SEPARATOR."logout.html");
 
+$menu = new UserMenu();
+$content = "";
+if (isset($_SESSION['email'])) {        
+  $content = $menu->getWelcomeMessage($_SESSION['email']);
+} else {
+  $content = $menu->getAuthenticationButtons();
+}
+
 if (session_unset()) {
+    $html = str_replace("<UserPlaceholder />", $content, $html);
     echo $html;
     header("refresh:2;Location: /");
 }else{
