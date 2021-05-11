8d35f04a215e18e226831b0c2a171da161d8acc0
br3sc4 <30345822+br3sc4@users.noreply.github.com>
2021-05-11 16:38:39 +0200

    Fix

---
+++ prenota.php
@@ -0,0 +1,18 @@
+<?php
+require_once __DIR__.DIRECTORY_SEPARATOR."php".DIRECTORY_SEPARATOR."UserMenu.php";
+
+session_start();
+
+$html = file_get_contents(__DIR__.DIRECTORY_SEPARATOR."pages".DIRECTORY_SEPARATOR."prenota.html");
+
+$menu = new UserMenu();
+$content = "";
+if (isset($_SESSION['email'])) {        
+  $content = $menu->getWelcomeMessage($_SESSION['email']);
+} else {
+  $content = $menu->getAuthenticationButtons();
+}
+
+$html = str_replace("<UserPlaceholder />", $content, $html);
+echo $html;
+?>
