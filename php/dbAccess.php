d95d00b8c64a4d9430f2e80d80cfa90af92eeba4
br3sc4 <30345822+br3sc4@users.noreply.github.com>
2021-05-11 10:54:44 +0200

    Fix login & signup

--- php/dbAccess.php
+++ php/dbAccess.php
@@ -1,4 +1,8 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 class DBAccess {
     private const HOST_DB = "127.0.0.1";
     private const USERNAME = "lbrescan";
