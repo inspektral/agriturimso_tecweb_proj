f3687cad4f67fd82922ef749e17a075df4083e53
br3sc4 <30345822+br3sc4@users.noreply.github.com>
2021-05-14 18:29:29 +0200

    Add dynamic news on index

--- php/dbAccess.php
+++ php/dbAccess.php
@@ -1,7 +1,7 @@
 <?php
-/*ini_set('display_errors', 1);
+ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);*/
+error_reporting(E_ALL);
 
 class DBAccess {
     private const HOST_DB = "127.0.0.1";
