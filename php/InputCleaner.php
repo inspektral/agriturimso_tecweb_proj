ffd2a708bb494b0105f1014068b769202d5f6d20
br3sc4 <30345822+br3sc4@users.noreply.github.com>
2021-05-15 16:30:34 +0200

    Add insert news

--- php/InputCleaner.php
+++ php/InputCleaner.php
@@ -1,6 +1,6 @@
 <?php
 class InputCleaner {
-  private const ALLOWED_NEWS_TAGS = ['em', 'strong', 'span'];
+  private const ALLOWED_NEWS_TAGS = "<em><strong><span>";
 
   public function cleanNews($input) {
     return htmlentities(strip_tags(trim($input), self::ALLOWED_NEWS_TAGS));
