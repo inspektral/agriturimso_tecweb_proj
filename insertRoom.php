429480d527472eada896e714205a3579ca4992f8
br3sc4 <30345822+br3sc4@users.noreply.github.com>
2021-05-18 15:18:21 +0200

    Add room form

--- insertRoom.php
+++ insertRoom.php
@@ -70,6 +70,7 @@ if (isset($_POST["submit"])) {
       $result = $dbAccess->addRoom($name,$people,$price,$mainImg,$imgLongdesc,$firstGallery,$secondGallery,$thirdGallery,$fourthGallery,$servicesBool);
       $dbAccess->closeDBConnection(); 
 
+      var_dump($result);
       if (!$result) {
         header("Location: /errors/500.php");
       }
