1c52d3d088f06d5557f3e7fb867bfd5c4834dd0c
TitoScutari <titoscutari@gmail.com>
2021-05-24 16:38:41 +0200

    validate signup form

--- inputCheck.js
+++ inputCheck.js
@@ -1,13 +1,17 @@
 var dettagliForm = {
   "da": ["Da","(0[1-9]|[12][0-9]|3[0-1])\/(0[0-9]|1[012])\/((20|19)[0-9][0-9])", "inserire la data nel formato gg/mm/aaaa"],
   "a": ["A","(0[1-9]|[12][0-9]|3[0-1])\/(0[0-9]|1[012])\/((20|19)[0-9][0-9])", "inserire la data nel formato gg/mm/aaaa"],
-  "nome": ["Nome","([a-zA-Z])(\ )([a-zA-Z]){2,20}","Il nome non può essere vuoto"],
-  "cognome":["Cognome","([a-zA-Z])(\ )([a-zA-Z]){2,20}","Il cognome non può essere vuoto"],
-  "email":["Email",,"Indirizzo email non valido"],
-  "password":["Password",,"La password deve avere almeno 8 caratteri"],
+  "nome": ["Nome","([a-zA-Z]){2,20}","Il nome non può essere vuoto"],
+  "cognome":["Cognome","([a-zA-Z]){2,20}","Il cognome non può essere vuoto"],
+  "email":["Email",/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,"Indirizzo email non valido"],
+  "password":["Password",/^.{8,}/,"La password deve avere almeno 8 caratteri"],
   "testo":["Testo","\.{10,}","Inserisci il tuo commento"],
 }
 
+function falsifier(){
+  console.log("falsifier called")
+  return false
+}
 
 function validatePrenota() {
   try {
@@ -21,9 +25,11 @@ function validatePrenota() {
       }
       return false
     }
+    return false
   }
-  catch {}
-  return false
+  catch {
+    return false
+  }
 }
 
 function validateComment() {
@@ -36,14 +42,42 @@ function validateComment() {
   }
 }
 
+function validateRegistrati() {
+  try {
+    console.log("validateRegistrati called");
+    const nome = document.getElementById("nome")
+    const cognome = document.getElementById("cognome")
+    const validNome = validateField(nome)
+    const validCognome = validateField(cognome)
+    const validCredents = validateAccedi()
+    return validNome && validCognome && validCredents
+  }
+  catch {
+    return false
+  }
+}
+
+function validateAccedi() {
+  try {
+    const email = document.getElementById("email")
+    const password = document.getElementById("password")
+    const validEmail = validateField(email)
+    const validPassword = validateField(password)
+    return validEmail && validPassword
+  }
+  catch {
+    return false
+  }
+}
+
  function validateField(input) {
-  let parent = input.parentNode;
+  const parent = input.parentNode;
 
 	if(parent.children.length == 2){
 		parent.removeChild(parent.children[1]);
 	}
-	let regex = dettagliForm[input.id][1];
-	let text = input.value;
+	const regex = dettagliForm[input.id][1];
+	const text = input.value;
 
 	if(text.search(regex) != 0){
 		showError(input, dettagliForm[input.id][2]);
