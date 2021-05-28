4354d943f4cd45ea1937f608d985ccc0a9b1be60
br3sc4 <30345822+br3sc4@users.noreply.github.com>
2021-05-28 16:59:53 +0200

    Add rooms list

--- php/dbAccess.php
+++ php/dbAccess.php
@@ -1,8 +1,8 @@
 <?php
-// ini_set('display_errors', 1);
-// ini_set('display_startup_errors', 1);
-// mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-// error_reporting(E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+error_reporting(E_ALL);
 
 class DBAccess {
     private const HOST_DB = "127.0.0.1";
