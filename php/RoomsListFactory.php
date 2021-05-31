70985124c53f735b71b41928dec0e92bac9adc5a
br3sc4 <30345822+br3sc4@users.noreply.github.com>
2021-05-31 11:44:15 +0200

    Fix accessibility & validation

--- php/RoomsListFactory.php
+++ php/RoomsListFactory.php
@@ -29,7 +29,7 @@ class RoomsListFactory {
         $roomsContent .= "<div class=\"bedroom\">";
         $roomsContent .= "<h2>$name</h2>";
         $roomsContent .= "<h3><span>$people persone</span> ";
-        $roomsContent .= "<span><abbr title=\"euro\">€</abbr> <span class=\"price\">$price</span> a notte";
+        $roomsContent .= "<span><abbr title=\"euro\">€</abbr></span> <span class=\"price\">$price</span> a notte";
         $roomsContent .= "</h3>";
         $roomsContent .= "<img src=\"$mainImg\" alt=\"\" longdesc=\"$mainImgLongdesc\" />";
         $roomsContent .= "<ul>";
