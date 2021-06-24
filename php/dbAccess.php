485498ab330e125798aa49a68ec5480813f3fec4
br3sc4 <30345822+br3sc4@users.noreply.github.com>
2021-06-24 12:55:14 +0200

    Fix

--- php/dbAccess.php
+++ php/dbAccess.php
@@ -1,4 +1,8 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+error_reporting(E_ALL);
 class DBAccess {
     private const HOST_DB = "localhost";
     private const USERNAME = "lbrescan";
